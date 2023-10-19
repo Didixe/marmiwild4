dde5789c2d0c87f2654c5dfb033b4807127846d2
Didixe <Adeline.gilet@gmail.com>
2023-10-19 19:39:11 +0200

    first commit

--- src/Controllers/RecipeController.php
+++ src/Controllers/RecipeController.php
@@ -1,22 +1,37 @@
 <?php
 
-require __DIR__ . '/../Models/RecipeModel.php';
+namespace App\Controllers;
+
+use App\Models\RecipeModel;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class RecipeController
 {
     private RecipeModel $model;
+    private Environment $twig;
 
     public function __construct()
     {
         $this->model = new RecipeModel();
+        $loader = new FilesystemLoader(__DIR__ . '/../Views/');
+        $this->twig = new Environment($loader);
     }
-
-    public function browse(): void
+    
+    public function browse(): string
     {
-        $recipes = $this->model->getAll();
-
-        require __DIR__ . '/../Views/indexRecipe.php';
+    $recipes = $this->model->getAll();
+    return $this->twig->render('indexRecipe.html.twig', [
+        'recipes' => $recipes
+    ]);
     }
+
+    // public function browse(): void
+    // {
+    //     $recipes = $this->model->getAll();
+
+    //     require __DIR__ . '/../Views/indexRecipe.php';
+    // }
 
     public function show(int $id)
     {
@@ -35,12 +50,16 @@ class RecipeController
         }
 
         // Generate the web page
-        require __DIR__ . '/../Views/showRecipe.php';
+        return $this->twig->render('showRecipe.html.twig', [
+            'recipe' => $recipe
+        ]);
+        // require __DIR__ . '/../Views/showRecipe.php';
     }
 
     public function add()
     {
         $errors = [];
+        $recipe = $this->model->getAll();
 
         if ($_SERVER["REQUEST_METHOD"] === 'POST') {
             $recipe = array_map('trim', $_POST);
@@ -56,7 +75,10 @@ class RecipeController
         }
 
         // Generate the web page
-        require __DIR__ . '/../Views/form.php';
+        return $this->twig->render('form.html.twig', [
+            'recipe' => $recipe
+        ]);
+        // require __DIR__ . '/../Views/form.php';
     }
 
     private function validate(array $recipe)
@@ -99,6 +121,9 @@ class RecipeController
         }
 
         // Generate the web page
-        require __DIR__ . '/../Views/form.php';
+        return $this->twig->render('form.html.twig', [
+            'recipe' => $recipe
+        ]);
+        // require __DIR__ . '/../Views/form.php';
     }
 }
--- src/routing.php
+++ src/routing.php
@@ -1,19 +1,17 @@
 <?php
 
-require __DIR__ . '/Controllers/RecipeController.php';
-
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$recipeController = new RecipeController();
+$recipeController = new App\Controllers\RecipeController();
 if ('/' === $urlPath) {
-    $recipeController->browse();
+    echo $recipeController->browse();
 } elseif ('/show' === $urlPath && isset($_GET['id'])) {
-    $recipeController->show($_GET["id"]);
+    echo $recipeController->show($_GET["id"]);
 } elseif ('/add' === $urlPath) {
-    $recipeController->add();
+    echo $recipeController->add();
 } elseif ('/delete' === $urlPath && isset($_GET['id']) ) {
-    $recipeController->delete($_GET["id"]);
+    echo $recipeController->delete($_GET["id"]);
 } elseif ('/update' === $urlPath && isset($_GET['id']) ) {
-    $recipeController->update($_GET["id"]);
+    echo $recipeController->update($_GET["id"]);
 }
  else {
     header('HTTP/1.1 404 Not Found');
